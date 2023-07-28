cc45f1e50888334dcfb6f885f11ac7e9a22b02de
QM M <762577099@qq.com>
2023-07-28 09:14:29 +0800

    第一个多线程

--- ch1_concurrency/main.cpp
+++ ch1_concurrency/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <thread>
 
+void hello(){
+    std::cout << "hello concurrency:"<< std::this_thread::get_id() << std::endl;
+}
 int main(){
-    std::cout << "hello concurrency" << std::endl;
+    std::cout << "hello begin:" << std::this_thread::get_id() << std::endl;
+    std::thread t(hello);
+    t.join();
+    std::cout << "hello end:"<< std::this_thread::get_id() << std::endl;
     return 1;
 }
