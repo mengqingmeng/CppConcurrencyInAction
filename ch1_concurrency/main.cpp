82ffbf5ea5fde4e6bedcc52a0445db6cdcf5dc6d
QM M <762577099@qq.com>
2024-07-03 17:55:10 +0800

    thread

--- ch1_concurrency/main.cpp
+++ ch1_concurrency/main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include <thread>
 
+// 入口函数
 void hello(){
     std::cout << "hello concurrency,thread id:"<< std::this_thread::get_id() << std::endl;
 }
+
 int main(){
     std::cout << "main begin,thread id:" << std::this_thread::get_id() << std::endl;
     
+    // 创建新的线程
     std::thread t(hello);
+    // 汇入主线程，等待t执行完成，再执行后面的代码
     t.join();
 
     std::cout << "main end,thread id:"<< std::this_thread::get_id() << std::endl;
-    return 1;
+
+    // 使用lambda运行线程
+    std::thread t2([]{
+        std::cout << "run thread2 in lambda,thread id:" << std::this_thread::get_id() << std::endl;
+    });
+
+    t2.join();
+
+    std::cout << "main end,thread id:"<< std::this_thread::get_id() << std::endl;
+    
+    return 0;
 }
